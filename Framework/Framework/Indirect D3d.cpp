140feaaeecbf556249ed7d1345a1d68dc4f4e839
ianaesthetic <ianaesthetic@live.com>
2017-10-20 21:04:07 +0800

    new

--- Framework/Framework/Indirect D3d.cpp
+++ Framework/Framework/Indirect D3d.cpp
@@ -1,21 +1,19 @@
 #include "d3Util.h"
+#include "d3dApp.h"
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-	switch (msg) {
-	case WM_CLOSE:
-		DestroyWindow(hwnd);
-		break;
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-	default: 
-		return DefWindowProc(hwnd, msg, wParam, lParam);
-	}
-	return 0;
+class IndirectD3DAPP :public D3DAPP {
+	IndirectD3DAPP(HINSTANCE hIstance);
+	~IndirectD3DAPP();
+
+	bool Init(); 
+	void UpdateScene(float dt);
+	void DrawScene();
+	void OnResize();
+};
+
+IndirectD3DAPP::IndirectD3DAPP(HINSTANCE hInstance) : D3DAPP(hInstance) {
 }
 
-int CALLBACK WinMain(
-	HINSTANCE hInstance, HINSTANCE hPrevInstance,
-	LPSTR lpCmdLine, int nCmdShow) {
-	return 0; 
+bool IndirectD3DAPP::Init() {
+	return D3DAPP::Init();
 }
